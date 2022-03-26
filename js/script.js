0bf84a602a8463a465217a0f960fe2ffaddd2707
yusuf0786 <74721122+yusuf0786@users.noreply.github.com>
2022-03-26 20:32:00 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+// targetting the elements
+let book_name = document.querySelector("#book_title")
+let file_name = document.querySelectorAll(".para_parent input")
+let copy_btn = document.querySelector("input[type='submit']")
+let waqiyat_section_code = document.querySelector(".waqiyat_section_code")
+
+// running the function while inputing in the input
+book_name.addEventListener('input', myfunction)
+file_name[0].addEventListener('input', myfunction)
+
+/* in the function below, targetting the values of the targetted elements and putting the code text in the value of textarea */
+function myfunction() {
+
+    file_name = document.querySelectorAll(".para_parent input")
+    for(let i = 0; i <= file_name.length-1; i++){
+        file_name[i].addEventListener('input', myfunction)
+    }
+
+    // generated code inside elements
+    let waqiya_heading = document.querySelector("#waqiya")
+    let waqiya_heading2 = document.querySelector(".waqiya_pg h1")
+    waqiya_heading.innerHTML = book_name.value
+    waqiya_heading2.innerHTML = book_name.value
+    // for paragraph
+    let waqiya_paragraph = document.querySelectorAll(".waqiya_pg p")
+    for(let i = 0; i <= waqiya_paragraph.length-1; i++){
+        waqiya_paragraph[i].innerHTML = file_name[i].value
+    }
+
+
+    let mycode = document.querySelector("#mycode")
+    let generate_code = waqiyat_section_code.innerHTML
+
+    mycode.value = generate_code
+}
+myfunction()
+
+/* copieng the value inside the textarea using the copy_btn */
+copy_btn.addEventListener('click', function(e){
+  e.preventDefault()
+  copy_the_code()
+})
+
+/* below is the function which can, when run copy the value of targetted element */
+function copy_the_code() {
+  /* Get the text field */
+  var copyText = document.getElementById("mycode");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+}
+
+
+// adding neccesory elements
+let waqiya_pg = document.querySelector(".waqiya_pg")
+let para_parent = document.querySelector(".para_parent")
+
+let para_add_btn = document.querySelector(".add_para")
+let para_remove_btn = document.querySelector(".remove")
+
+
+para_add_btn.addEventListener('click', function(e){
+    e.preventDefault()
+    add_input_and_p()
+    myfunction()
+})
+para_remove_btn.addEventListener('click', function(e){
+    e.preventDefault()
+    remove_input_and_p()
+    myfunction()
+})
+
+function add_input_and_p() {
+    let input = document.createElement("input")
+    let p = document.createElement("p")
+    
+    input.setAttribute('type', 'text')
+    input.setAttribute('name', 'file_name')
+
+    para_parent.appendChild(input)
+    waqiya_pg.appendChild(p)
+}
+function remove_input_and_p() {
+    let input = document.querySelector(".para_parent input:last-child")
+    let p = document.querySelector(".waqiya_pg p:last-child")
+    input.remove()
+    p.remove()
+}
